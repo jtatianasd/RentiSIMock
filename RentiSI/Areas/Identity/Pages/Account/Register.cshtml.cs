a9e8e9f782a0c6964a41c8b6c92c8d711d4c940b
Dany Perez <danyohan10@hotmail.com>
2024-04-02 19:31:17 -0500

    Se corrige para que guarde el nombre en lugar de email al momento de crear un usuario

--- RentiSI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RentiSI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,7 +122,7 @@ namespace RentiSI.Areas.Identity.Pages.Account
 
                 user.Nombre = Input.Nombre;
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Nombre, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
