50cedd50c20bff7103d0dfa3476777d2ee8b5791
jtatianasd <jtatianasd@gmail.com>
2024-04-05 11:39:57 -0500

    Agregar modelo Gestion

---
+++ RentiSI.Modelos/Gestion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentiSI.Modelos
+{
+    public class Gestion
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? Id_Tramite { get; set; }
+
+        [ForeignKey("Id_Tramite")]
+        public Tramite? Id_Tramite_Gestion { get; set; }
+        [Display(Name = "Fecha de gestión del tramite")]
+        public string? FechaGestion { get; set; }
+        [Display(Name = "Estado gestion revisión")]
+        public string? EstadoGestion { get; set; }
+        public string? IdUsuarioGestion { get; set; }
+        [ForeignKey("IdUsuarioGestion")]
+        public ApplicationUser? UsuarioGestion { get; set; }
+        [Display(Name = "Fecha de resultado")]
+        public string? FechaResultado { get; set; }
+        public string? IdUsuarioResuelve { get; set; }
+
+        [ForeignKey("IdUsuarioResuelve")]
+        public ApplicationUser? UsuarioResuelve { get; set; }
+    }
+}
