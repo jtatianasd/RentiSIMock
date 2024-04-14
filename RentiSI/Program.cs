eae9b94265bf3db4a221081cddf28a23b5a734c6
Dany Perez <danyohan10@hotmail.com>
2024-04-13 20:48:40 -0500

    Cambios para 404

--- RentiSI/Program.cs
+++ RentiSI/Program.cs
@@ -36,12 +36,23 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+   // app.UseExceptionHandler("/Home/Error");
 }
+
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404)
+    {
+        context.Request.Path = "/error/404";
+        await next();
+    }
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
