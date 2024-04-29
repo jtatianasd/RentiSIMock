23c0b74c5ada16fead4d27a690f6d5fd018c552c
jtatianasd <jtatianasd@gmail.com>
2024-04-28 21:03:46 -0500

    Eliminar campos innecesarios del modelo de usuarios

---
+++ RentiSI.AccesoDatos/Migrations/20240429015916_RemoverCamposInnecesariosDeUsuario.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentiSI.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoverCamposInnecesariosDeUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ciudad",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Pais",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ciudad",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pais",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- RentiSI.Modelos/ApplicationUser.cs
+++ RentiSI.Modelos/ApplicationUser.cs
@@ -12,12 +12,7 @@ namespace RentiSI.Modelos
     {
         [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
-        [Required(ErrorMessage = "La Direccion es obligatoria")]
-        public string Direccion { get; set; }
-        [Required(ErrorMessage = "La Ciudad es obligatoria")]
-        public string Ciudad { get; set; }
-        [Required(ErrorMessage = "El pais es obligatorio")]
-        public string Pais { get; set; }
+
 
     }
 }
