51448bec21a85353e5fad4b98e8ead12af91dd35
Dany Perez <danyohan10@hotmail.com>
2024-04-28 21:13:48 -0500

    Cambios para el usuario

---
+++ RentiSI.Modelos/viewModels/UsuarioVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentiSI.Modelos.viewModels
+{
+    public class UsuarioVM
+    {
+        public ApplicationUser? Usuario { get; set; }
+
+        public string Rol { get; set; }
+    }
+}
--- RentiSI/Areas/Admin/Controllers/UsuariosController.cs
+++ RentiSI/Areas/Admin/Controllers/UsuariosController.cs
@@ -2,7 +2,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using RentiSI.AccesoDatos.Data.Repository.IRepository;
+using RentiSI.AccesoDatos.Migrations;
 using RentiSI.Modelos;
+using RentiSI.Modelos.viewModels;
+using System.Collections.Generic;
+using System.Data;
 using System.Security.Claims;
 
 namespace RentiSI.Areas.Admin.Controllers
@@ -14,6 +18,8 @@ namespace RentiSI.Areas.Admin.Controllers
         private readonly IContenedorTrabajo _contenedorTrabajo;
 
         private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<Role> _roleManager;
 
         public UsuariosController(IContenedorTrabajo contenedorTrabajo, UserManager<ApplicationUser> userManager)
         {
@@ -57,13 +63,96 @@ namespace RentiSI.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var user = await  _userManager.FindByIdAsync(id);
+            var user = await _userManager.FindByIdAsync(id);
             if (user == null)
             {
                 return NotFound();
             }
 
-            return View(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles == null)
+            {
+                return NotFound();
+            };
+
+            UsuarioVM usuarioVM = new UsuarioVM()
+            {
+                Usuario = user,
+                Rol = roles.FirstOrDefault()
+            };
+
+            return View(usuarioVM);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UsuarioVM usuarioVM)
+        {
+            IdentityResult result = default;
+
+            if (ModelState.IsValid)
+            {
+                var user = _userManager.FindByIdAsync(usuarioVM.Usuario.Id);
+                if (user != null)
+                {
+
+                    if (usuarioVM.Usuario.PasswordHash != null)
+                    {
+                        var token = await _userManager.GeneratePasswordResetTokenAsync(user.Result);
+                        result = await _userManager.ResetPasswordAsync(user.Result, token, usuarioVM.Usuario.PasswordHash);
+
+                    }
+
+                    var rol = ObtenerRoles(usuarioVM.Usuario.Id).Result;
+
+                    var respuestaEliminarRol = await RemoveRoleFromUserAsync(user, rol.FirstOrDefault());
+                    if (respuestaEliminarRol)
+                    {
+                        await AgregarRol(user, usuarioVM.Rol);
+                    }
+
+                    user.Result.Nombre = usuarioVM.Usuario.Nombre;
+                    user.Result.UserName = usuarioVM.Usuario.UserName;
+                    var usuario = await _userManager.UpdateAsync(user.Result);
+                    if (usuario.Succeeded)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+            foreach (var campo in ModelState.Keys)
+            {
+                var erroresCampo = ModelState[campo].Errors;
+                foreach (var error in erroresCampo)
+                {
+                    // Aquí puedes hacer lo que necesites con los errores, como registrarlos o mostrarlos en la vista
+                    var mensajeError = error.ErrorMessage;
+                    // Puedes hacer algo con el mensaje de error, como agregarlo a una lista para mostrar en la vista
+                }
+            }
+
+            return View();
+        }
+
+        private async Task<IList<string>> ObtenerRoles(string usuarioId)
+        {
+            var usuario = await _userManager.FindByIdAsync(usuarioId);
+            return await _userManager.GetRolesAsync(usuario);
+        }
+        private async Task<bool> RemoveRoleFromUserAsync(Task<ApplicationUser>usuarioId, string rol)
+        {
+            var result = await _userManager.RemoveFromRoleAsync(usuarioId.Result, rol);
+            return result.Succeeded;
+
+         }
+
+
+         private async Task<bool> AgregarRol(Task<ApplicationUser> usuario, string rol)
+         {
+             var resultado = await _userManager.AddToRoleAsync(usuario.Result, rol);
+             return resultado.Succeeded;
+        }
+
+
     }
 }
